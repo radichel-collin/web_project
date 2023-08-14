e40507267af24e756ebc0a0f498f87648538ab8e
Collin Radichel <collinradichel@Collins-MacBook-Pro.local>
2023-08-14 03:36:44 -0500

    experimenting with the Menu components in MUI

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,12 +3,15 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-// import Button from '@mui/material/Button';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-
+import Paper from '@mui/material/Paper';
+// import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 export default function ButtonAppBar() {
   const [expanded, setExpanded] = useState(false)
@@ -16,6 +19,13 @@ export default function ButtonAppBar() {
   const toggleExpand = () => {
     setExpanded(!expanded);
   };
+
+  const links = [
+'Link 1',
+'Link 2',
+'Link 3',
+'Link 4'
+  ];
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -36,11 +46,22 @@ export default function ButtonAppBar() {
             onClick={toggleExpand}
             aria-expanded={expanded} // Setting the aria-expanded attribute
             >
-              {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+              {expanded ? <ExpandMoreIcon /> : <ExpandLessIcon />}
           </IconButton>
           </Typography>
         </Toolbar>
       </AppBar>
-    </Box>
+      {expanded && (
+        <Paper>
+        <MenuList>
+          {links.map((link, index) => (
+            <MenuItem key={index}>
+              <ListItemText primary={link} />
+            </MenuItem>
+          ))}
+          </MenuList>
+         </Paper>
+      )}
+      </Box>
   );
 }
