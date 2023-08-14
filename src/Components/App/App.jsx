99fcd41bae03ffbab8b84dd8f598331566d718b1
Collin Radichel <collinradichel@Collins-MacBook-Pro.local>
2023-08-14 01:49:43 -0500

    installed MUI, started reworking the header component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,6 @@
 // import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "../Header/Header";
+import ButtonAppBar from "../Header/Header";
 import View1 from "../View1/View1";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Header />}/>
+          <Route path="/" element={<ButtonAppBar />}/>
 
           <Route path="/View1" element={<View1 />}/>
 
