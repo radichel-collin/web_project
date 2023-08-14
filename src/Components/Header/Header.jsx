b8aa64ae34a3cbda192e3a7ff3efbc7357d2b0cb
Collin Radichel <collinradichel@Collins-MacBook-Pro.local>
2023-08-14 02:39:48 -0500

    adding expandable content to the header, making sure aria attributes are set properly

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,22 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+
 
 export default function ButtonAppBar() {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -22,9 +31,14 @@ export default function ButtonAppBar() {
            <HomeRoundedIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Accessiblility Examples
+            <IconButton
+            onClick={toggleExpand}
+            aria-expanded={expanded} // Setting the aria-expanded attribute
+            >
+              {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </IconButton>
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
