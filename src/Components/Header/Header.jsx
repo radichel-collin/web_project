cf123f7fbdf71fccee3dc17ffe6681f18f6d68a7
collin-radichel <radichel.collin@gmail.com>
2023-08-14 22:19:56 -0500

    working on the collapsable list of links in the header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import MenuList from '@mui/material/MenuList';
-import MenuItem from '@mui/material/MenuItem';
+// import MenuList from '@mui/material/MenuList';
+// import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import Paper from '@mui/material/Paper';
-// import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 
 export default function ButtonAppBar() {
@@ -21,7 +23,7 @@ export default function ButtonAppBar() {
   };
 
   const links = [
-'Link 1',
+<Link to="/View1">View 1</Link>,
 'Link 2',
 'Link 3',
 'Link 4'
@@ -45,23 +47,26 @@ export default function ButtonAppBar() {
             <IconButton
             onClick={toggleExpand}
             aria-expanded={expanded} // Setting the aria-expanded attribute
+            aria-controls="link-list"
             >
               {expanded ? <ExpandMoreIcon /> : <ExpandLessIcon />}
           </IconButton>
           </Typography>
         </Toolbar>
       </AppBar>
+      <Paper
+        id="link-list"
+      >
       {expanded && (
-        <Paper>
-        <MenuList>
+        <List>
           {links.map((link, index) => (
-            <MenuItem key={index}>
+            <ListItem key={index}>
               <ListItemText primary={link} />
-            </MenuItem>
+            </ListItem>
           ))}
-          </MenuList>
-         </Paper>
+          </List> 
       )}
+      </Paper>
       </Box>
   );
 }
